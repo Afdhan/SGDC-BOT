c8ad5877bf6b5f42f1458beb6995e138b52768c7
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-26 21:38:10 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -219,3 +219,13 @@ handler.fail = null
 
 
 module.exports = handler
+
+function clockString(ms) {
+  let h = Math.floor(ms / 3600000)
+  let m = Math.floor(ms / 60000) % 60
+  let s = Math.floor(ms / 1000) % 60
+  console.log({ms,h,m,s})
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+}
+
+// Muhammad afdhan
