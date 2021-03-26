9c1a64340ce9f0ff9c59b731bfaa29152550920a
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-27 00:21:30 +0700

    Update randombts.js

--- plugins/randombts.js
+++ plugins/randombts.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
   await m.reply('*[ WAIT ]* _Sedang Diproses..._')
 let link = 'http://lolhuman.herokuapp.com/api/random/bts?apikey=761e676c13e7710a48011b2b'
 
-conn.sendFile(m.chat, link, 'SGDC-BOT.png', 'No Colay:v\n\n*[ • SGDC-BOT • ]*', m)
+conn.sendFile(m.chat, link, 'SGDC-BOT.png', 'Batues:v\n\n*[ • SGDC-BOT • ]*', m)
 }
 
 handler.command = /^(bts|randombts|batues)$/i
