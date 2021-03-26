589f09a9dc6b86b191298df936daba43f778685a
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-26 20:17:46 +0700

    Add files via upload

---
+++ plugins/randomloli.js
@@ -0,0 +1,26 @@
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+  await m.reply('*[ WAIT ]* _Sedang Diproses..._')
+let link = 'http://lolhuman.herokuapp.com/api/random/loli?apikey=761e676c13e7710a48011b2b'
+
+conn.sendFile(m.chat, link, 'SGDC-BOT.png', 'No Colay:v\n\n*[ • SGDC-BOT • ]*', m)
+}
+
+handler.command = /^(loli|randomloli)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+
+module.exports = handler
+
+
+//   MUHAMMAD AFDHAN
