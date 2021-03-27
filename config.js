5e6a358bd475a404023fd064b2a0d0ef30ceceae
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-27 17:49:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 global.owner = ['6282252655313'] // Put your number here
-global.mods = ['6282252655313']
-global.prems = ['6282252655313']
+global.mods = ['6282252655313]
+global.prems = []
 global.APIs = { 
   nrtm: 'https://nurutomo.herokuapp.com',
   xteam: 'https://api.xteam.xyz'
