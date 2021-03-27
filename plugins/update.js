ea33cbf2f4df8edd50fe2ce82d4a8f9ef7db9881
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-27 14:40:53 +0700

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,6 +1,6 @@
 let { execSync } = require('child_process')
 let handler = async (m, { conn, text }) => {
- await m.reply('*_Memulai Ulang BOT..._*')
+ await m.reply('*_Sedang Merestart BOT..._*')
   if (global.conn.user.jid == conn.user.jid) {
  // conn.reply(m.chat, '*_Memulai Ulang BOT..._*', m)
     let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
