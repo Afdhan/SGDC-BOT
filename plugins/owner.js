8462dc80dd3ba7edfa023d06a9ce385140032503
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-27 16:34:47 +0700

    Add files via upload

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,17 +1,20 @@
-let util = require('util')
-let path = require('path')
-let { spawn } = require('child_process')
-
-let handler = function (m) => m
-let own = /^(owner|creator|pemilik)$/i
- handler.before = m => {
-  if (m.isBaileys && m.fromMe) return true
-  let chat = global.DATABASE.data.chats[m.chat]
-  let isOwner = own.exec(m.text)
-  if (isOwner) this.sendContact(m.chat, '6282252655313', '꧁࿇ ᴍ ᴀꜰᴅʜᴀɴ ࿇꧂', m)
-  return true
+let handler = function (m) {
+  this.sendContact(m.chat, '6282252655313', '꧁࿇ ᴍ ᴀꜰᴅʜᴀɴ ࿇꧂', m)
+let pp_owner ='src/Owner.jpg'
+conn.sendFile(m.chat, pp_owner, 'My Owner.jpg', '*This is my owner ᴍ ᴀꜰᴅʜᴀɴ*\n*Contact: https://wa.me/6282252655313*', m)
 }
 
+handler.command = new  RegExp
+handler.customPrefix = /^(owner|creator|pemilik)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
 module.exports = handler
- 
-// M AFDHAN
