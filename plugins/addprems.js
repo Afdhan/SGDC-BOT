19989cfe4a448a4f4b378b2183cea93d230acb4d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-27 18:30:48 +0700

    Update addprems.js

--- plugins/addprems.js
+++ plugins/addprems.js
@@ -5,12 +5,12 @@ let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
   if (!who) throw 'Tag salah satu lah,dan masukkan nomor untuk di verivikasi !'
-  let participants = m.mentionedJid[0]
-  if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
+ // let participants = m.mentionedJid[0]
+ // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].premium = true
   var nomor = m.mentionedJid[0]
     m.reply(`*_Berhasil Menambahkan User!_*\n*Nomor : https://wa.me/${nomor.split("@s.whatsapp.net")[0]}*`)
-   } else m.reply('Ada nomor host disini...')
+   //} else m.reply('Ada nomor host disini...')
 }
   
 handler.command = /^addprem$/i
