ed3520cf0d30672cbac15a877aaf92ac21c3fa35
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-27 16:37:08 +0700

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
   this.sendContact(m.chat, '6282252655313', '꧁࿇ ᴍ ᴀꜰᴅʜᴀɴ ࿇꧂', m)
-let pp_owner ='src/Owner.jpg'
-conn.sendFile(m.chat, pp_owner, 'My Owner.jpg', '*This is my owner ᴍ ᴀꜰᴅʜᴀɴ*\n*Contact: https://wa.me/6282252655313*', m)
+//let pp_owner ='src/Owner.jpg'
+//conn.sendFile(m.chat, pp_owner, 'My Owner.jpg', '*This is my owner ᴍ ᴀꜰᴅʜᴀɴ*\n*Contact: https://wa.me/6282252655313*', m)
 }
 
 handler.command = new  RegExp
