3ab83fff52803bd39e4332d293a8398f3b78824d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-28 08:30:46 +0700

    Update cewek.js

--- plugins/cewek.js
+++ plugins/cewek.js
@@ -36,7 +36,7 @@ Jangan Ngaceng :v
 handler.command = /^(cecan|ciwi|cewek)$/i
 handler.owner = false
 handler.mods = false
-handler.premium = false
+handler.premium = true
 handler.group = false
 handler.private = false
 
