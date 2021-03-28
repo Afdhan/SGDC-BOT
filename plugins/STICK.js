4da07685dc3a3231e5265d7365177d5414164320
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-28 19:46:04 +0700

    Add files via upload

---
+++ plugins/STICK.js
@@ -0,0 +1,43 @@
+let imageToBase64 = require('image-to-base64');
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+await m.reply('*[ WAIT ]* _Sedang Diproses..._')
+
+        let url = 'https://api.xteam.xyz/sticker/stickpack?q=' + text + '&APIKEY=abba3220ce4a347f'
+axios.get(url)
+      .then((result) => {
+        let b = JSON.parse(JSON.stringify(result.data));
+        let cewek =  b[Math.floor(Math.random() * b.length)];
+        imageToBase64(cewek)
+        .then(
+            (response) => {
+	let buf = Buffer.from(response, 'base64');
+    let spn = encodeURIComponent(buf)
+    conn.sendFile(m.chat, spn, 'sponge.webp', '', m, false, { asSticker: true })
+            }
+        )
+        .catch(
+            (error) => {
+                console.log(error); 
+            }
+        )
+    
+    });
+    }
+handler.command = /^(ssearch)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+
+module.exports = handler
+
+// MUHAMMAD AFDHAN
