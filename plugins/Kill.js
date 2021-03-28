11e91d71443dc8b6aa9ae09ce8ad06f122d98be0
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-28 18:52:10 +0700

    Update Kill.js

--- plugins/Kill.js
+++ plugins/Kill.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, args }) => {
 }
 
 handler.command = new RegExp
-handler.customPrefix = /^(Kill|kill|ulti|Ulti)$/i
+handler.customPrefix = /(Kill|kill|ulti|Ulti)/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
