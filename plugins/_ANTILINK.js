a2347daaeb39ffb78393bc944038871e9e723e23
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-28 17:02:26 +0700

    Update _ANTILINK.js

--- plugins/_ANTILINK.js
+++ plugins/_ANTILINK.js
@@ -6,9 +6,11 @@ let handler = async function(m, { conn , args, isAdmin, isBotAdmin }) {
   let name = m.fromMe ? conn.user : conn.contacts[m.sender]
   let users = m.sender
   let chat = global.DATABASE.data.chats[m.chat]
- if (chat.antiLink) return conn.reply(m.chat, `*「 ANTI LINK 」*\n\nTerdeteksi *${usname}* telah mengirim link group!\n\nMaaf Kamu akan dikick oleh *[ • SGDC-BOT • ]*`, m)
+ if (chat.antiLink) { 
+   conn.reply(m.chat, `*「 ANTI LINK 」*\n\nTerdeteksi *${usname}* telah mengirim link group!\n\nMaaf Kamu akan dikick oleh *[ • SGDC-BOT • ]*`, m)
  await conn.groupRemove(m.chat, [users])
-}
+    }
+ }
 
 handler.customPrefix = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 handler.command = new RegExp
