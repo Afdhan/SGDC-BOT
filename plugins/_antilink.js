2f69986615ebe3f0e19256625acaca1f0f38d121
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-03-28 14:08:19 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,18 +1,22 @@
-let handler = m => m
+let handler = async function(m, { conn , args, isAdmin, isBotAdmin }) {
 
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
-handler.before = m => {
-  if (m.isBaileys && m.fromMe) return true
-  let chat = global.DATABASE.data.chats[m.chat]
-  let isGroupLink = linkRegex.exec(m.text)
-  if (chat.antiLink && isGroupLink) {
-    m.reply('*_Kick !?_*')
-  if (global.opts['restrict']) {
-       if (!user.isAdmin) return true
-       this.groupRemove(m.chat, [m.sender])
-    }
-  }
-  return true
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let usname = conn.getName(who)
+  let mentionedJid = [m.sender]
+  let name = m.fromMe ? conn.user : conn.contacts[m.sender]
+  let users = m.sender
+
+    conn.reply(m.chat, `*「 ANTI LINK 」*\n\nTerdeteksi *${usname}* telah mengirim link group!\n\nMaaf Kamu akan dikick oleh *[ • SGDC-BOT • ]*`, m)
+ await conn.groupRemove(m.chat, [users])
 }
+handler.tags = ['group']
+handler.customPrefix = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.command = new RegExp
+handler.group = true
+
+handler.admin = false
+handler.botAdmin = true
+
+handler.fail = null
 
 module.exports = handler
